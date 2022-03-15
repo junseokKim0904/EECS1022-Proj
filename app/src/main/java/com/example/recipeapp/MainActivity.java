fdf003016811ce32474331dd5276306623cec7dc
junseokKim <junkim@my.yorku.ca>
2022-03-15 13:03:53 -0400

    a button to return to Main, Main & recipe01 linked

--- app/src/main/java/com/example/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.receipe01_bt);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, recipe01_Activity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/recipeapp/recipe01_Activity.java
+++ app/src/main/java/com/example/recipeapp/recipe01_Activity.java
@@ -2,6 +2,7 @@ package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,15 +12,24 @@ import org.w3c.dom.Text;
 
 public class recipe01_Activity extends AppCompatActivity {
 
+    private Button button;
+
     Button inc, dec;
     TextView bowls;
     int count;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe01);
+
+        button = (Button) findViewById(R.id.return_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
 
         inc = findViewById(R.id.increment);
         dec = findViewById(R.id.decrement);
@@ -41,6 +51,9 @@ public class recipe01_Activity extends AppCompatActivity {
         });
     }
 
-
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
 }
