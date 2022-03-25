4b5c7fa5659db892f7c4fa5b507086d9aa224bf5
junseokKim <junkim@my.yorku.ca>
2022-03-24 22:11:18 -0400

    added more layouts up to 10

--- app/src/main/java/com/example/recipeapp/KeywordList.java
+++ app/src/main/java/com/example/recipeapp/KeywordList.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 public class KeywordList extends AppCompatActivity {
 
     private Button key_bt;
+    private Button recipe0101_bt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +26,23 @@ public class KeywordList extends AppCompatActivity {
                 openMainActivity();
             }
         });
+
+        recipe0101_bt = (Button) findViewById(R.id.kw_recipe_01_01_bt);
+        recipe0101_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRecipe0101();
+            }
+        });
     }
 
     public void openMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openRecipe0101(){
+        Intent intent = new Intent(this, recipe01_Activity.class);
         startActivity(intent);
     }
 }
