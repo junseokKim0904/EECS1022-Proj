242353cac7df11f6a56e41a664d636db1182c0d0
junseokKim <junkim@my.yorku.ca>
2022-03-27 12:30:16 -0400

    matched the size of each txt box and adding recipe 6 and 7

--- app/src/main/java/com/example/recipeapp/recipe01_Activity.java
+++ app/src/main/java/com/example/recipeapp/recipe01_Activity.java
@@ -3,12 +3,19 @@ package com.example.recipeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CheckedTextView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.nio.charset.CharacterCodingException;
+import java.util.prefs.Preferences;
 
 public class recipe01_Activity extends AppCompatActivity {
 
@@ -17,6 +24,7 @@ public class recipe01_Activity extends AppCompatActivity {
     Button inc, dec;
     TextView bowls;
     int count;
+    CheckedTextView C1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +57,8 @@ public class recipe01_Activity extends AppCompatActivity {
                 bowls.setText(count);
             }
         });
+
+        CheckBox ingred_cb01 = (CheckBox) findViewById(R.id.ingred_cb01);
     }
 
     public void openMainActivity(){
